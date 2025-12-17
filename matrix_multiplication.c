8489adb48b2dd1a6bc92c3f44619e32dd95ce31d
anantrajputcode <anantr2006@gmail.com>
2025-12-17 11:49:16 +0530

    modified code now it can even calulate determinant

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -1,62 +1,117 @@
 #include <stdio.h>
 int main()
-{
-    unsigned int r1, c1, r2, c2;
-    printf("ENTER THE NUMBER OF ROWS OF MATRIX A:-\n");       //Taking input for the dimensions of matrix A and B and using unsigned int to make sure dimension of matrix are not negative.
-    scanf("%u", &r1);
-    printf("\nENTER THE NUMBER OF COLUMN OF MATRIX A:-\n");
-    scanf("%u", &c1);
-    printf("\nENTER THE NUMBER OF ROWS IN MATRIX B:-\n");
-    scanf("%u", &r2);
-    printf("\nENTER THE NUMBER OF COLUMN IN MATRIX B:-\n");
-    scanf("%u", &c2);
-    if (r1 == 0 || c1 == 0 || r2 == 0 || c2 == 0)            //Checking rows and column are not equal to zero.
+{   
+    printf("WELCOME TO MATOPS\n");
+    printf("__________________\n");
+    int choice;
+    printf("CHOOSE OPERATIONS:\n");
+    printf("1. MATRIX MULTIPLICATION\n");
+    printf("2. DETERMINANT\n");
+    printf("ENTER YOUR CHOICE:");
+    scanf("%d", &choice);
+    if (choice == 1)
     {
-        printf("\nROWS AND COLUMNS MUST BE GREATER THAN ZERO.\n");
-    }
-    else if (c1 != r2)
-    {
-        printf("\nSORRY! THE MULTIPLICATION OF THE MATRIX IS NOT POSSIBLE.");
-    }
-    else
-    {
-        int arr1[r1][c1],arr2[r2][c2],arr3[r1][c2], i, j, k;
-        printf("\nENTER THE ELEMENTS OF MATRIX A:-\n");
-        
-        //Input Matrix A
-        for (i = 0; i < r1; i++)
+        unsigned int r1, c1, r2, c2;
+        printf("ENTER THE NUMBER OF ROWS OF MATRIX A:-\n");       //Taking input for the dimensions of matrix A and B and using unsigned int to make sure dimension of matrix are not negative.
+        scanf("%u", &r1);
+        printf("\nENTER THE NUMBER OF COLUMN OF MATRIX A:-\n");
+        scanf("%u", &c1);
+        printf("\nENTER THE NUMBER OF ROWS IN MATRIX B:-\n");
+        scanf("%u", &r2);
+        printf("\nENTER THE NUMBER OF COLUMN IN MATRIX B:-\n");
+        scanf("%u", &c2);
+        if (r1 == 0 || c1 == 0 || r2 == 0 || c2 == 0)            //Checking rows and column are not equal to zero.
         {
-            for (j = 0; j < c1; j++)
-            {
-                scanf("%d", &arr1[i][j]);
-            }
+            printf("\nROWS AND COLUMNS MUST BE GREATER THAN ZERO.\n");
         }
-        printf("\nENTER THE ELEMENTS OF MATRIX B:-\n");
-        
-        //Input Matrix B
-        for (i = 0; i < r2; i++)
+        else if (c1 != r2)
         {
-            for (j = 0; j < c2; j++)
-            {
-                scanf("%d", &arr2[i][j]);
-            }
+            printf("\nSORRY! THE MULTIPLICATION OF THE MATRIX IS NOT POSSIBLE.");
         }
-        printf("\nTHE RESULT OF MULTIPLICATION OF MATRIX A AND MATRIX B IS:-\n");
-        for (i = 0; i < r1; i++)
-        {   
+        else
+        {
+            int arr1[r1][c1],arr2[r2][c2],arr3[r1][c2], i, j, k;
+            printf("\nENTER THE ELEMENTS OF MATRIX A:-\n");
             
-            for (j = 0; j < c2; j++)                     //resultant matrix
+            //Input Matrix A
+            for (i = 0; i < r1; i++)
             {
-                arr3[i][j] = 0;
-                for (k = 0; k < c1; k++)
+                for (j = 0; j < c1; j++)
                 {
-                    arr3[i][j] += arr1[i][k] * arr2[k][j];
+                    scanf("%d", &arr1[i][j]);
                 }
-                printf("%d\t", arr3[i][j]);
             }
-            printf("\n");
-
+            printf("\nENTER THE ELEMENTS OF MATRIX B:-\n");
+            
+            //Input Matrix B
+            for (i = 0; i < r2; i++)
+            {
+                for (j = 0; j < c2; j++)
+                {
+                    scanf("%d", &arr2[i][j]);
+                }
+            }
+            printf("\nTHE RESULT OF MULTIPLICATION OF MATRIX A AND MATRIX B IS:-\n");
+            for (i = 0; i < r1; i++)
+            {   
+                
+                for (j = 0; j < c2; j++)                     //resultant matrix
+                {
+                    arr3[i][j] = 0;
+                    for (k = 0; k < c1; k++)
+                    {
+                        arr3[i][j] += arr1[i][k] * arr2[k][j];
+                    }
+                    printf("%d\t", arr3[i][j]);
+            }
+                printf("\n");
+    
+            }
         }
     }
+    else if (choice == 2)
+    {
+        unsigned int r1, c1;
+        printf("ENTER THE NUMBER OF ROWS OF MATRIX A:-\n");       //Taking input for the dimensions of matrix A and B and using unsigned int to make sure dimension of matrix are not negative.
+        scanf("%u", &r1);
+        printf("\nENTER THE NUMBER OF COLUMN OF MATRIX A:-\n");
+        scanf("%u", &c1);
+        int arr1[r1][c1];
+        if (r1 != c1)
+        {
+            printf("DETERMINANT DOES NOT EXIST\n");
+        }
+        else if (r1 == c1)
+        {
+            printf("ENTER THE ELEMENTS OF MAT A:\n");
+            for (int i = 0; i < r1; i++)
+            {
+                for (int j = 0; j < c1; j++)
+                {
+                    scanf("%d", &arr1[i][j]);
+                }
+            }
+            int detA;
+            if (r1 == 1)
+            {
+                detA = arr1[0][0];
+                printf("DETERMINANAT OF MATRIX A IS : %d", detA);
+            }
+            else if (r1 == 2)
+            {
+                detA = arr1[0][0] * arr1[1][1] - (arr1[0][1] * arr1[1][0]);
+                printf("DETERMINANT OF MATRIX A IS : %d", detA); 
+            }
+            else if (r1 == 3)
+            {
+                detA = arr1[0][0]*(arr1[1][1]*arr1[2][2] - arr1[1][2]*arr1[2][1])
+                      - arr1[0][1]*(arr1[1][0]*arr1[2][2] - arr1[1][2]*arr1[2][0])
+                      + arr1[0][2]*(arr1[1][0]*arr1[2][1] - arr1[1][1]*arr1[2][0]);
+                printf("DETERMINANT OF MATRIX A IS :%d", detA);
+
+            }
+        }
+
+    }    
     return 0;
 }
